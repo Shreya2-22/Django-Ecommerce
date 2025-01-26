671da8ba107bbc6d59035afc2533e83b4516c068
Shreya2-22 <kshreya22@tbc.edu.np>
2025-01-26 16:00:00 +0545

    Update settings

--- ecommproject/settings.py
+++ ecommproject/settings.py
@@ -97,7 +97,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'shreya23khadka@gmail.com'
-EMAIL_HOST_PASSWORD = 'dost erqm lbsg qgvj'  # Use your App Password here
+EMAIL_HOST_USER = 'yourmail@gmail.com'
+EMAIL_HOST_PASSWORD = 'yourpassword'  # Use your App Password here
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
